f4f9bf838aa339343d7100c260f6f329f0083e2b
loomy2021 <stev@loomcomputer.com>
2024-07-17 22:04:18 +0200

    set square and minor changes

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import {s} from "./App.style";
-import {  Text, View } from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context";
+import { Home } from "./pages/Home/Home";
+import { Part2 } from "./pages/Part2/Part2";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {Text, TouchableOpacity} from "react-native";
 
+const Stack = createNativeStackNavigator();
+const navTheme = {
+    colors: {
+         //background: "transparent",
+         backgroundColor: "red"
+    }
+}
 
 export default function App() {
-  return (
-      <SafeAreaProvider>
-        <SafeAreaView style={s.container} >
+    return (
+        <NavigationContainer theme={navTheme}  >
+        <SafeAreaProvider >
+        <SafeAreaView style={[s.container]}>
+            <Stack.Navigator initialRouteName={Home} screenOptions={{headerShown: false, }}>
+            <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Part2" component={Part2} />
 
-            <View style={s.square}></View>
-
+            </Stack.Navigator>
         </SafeAreaView>
-      </SafeAreaProvider>
+            </SafeAreaProvider>
+        </NavigationContainer>
 
-
-  );
+    );
 }
+
+
 
--- App.style.js
+++ App.style.js
@@ -3,13 +3,8 @@ import { StyleSheet } from 'react-native';
 export const s = StyleSheet.create({
     container: {
         flex:1,
-
-
         justifyContent: 'center',
     },
-    square: {
-        width: 50,
-        height: 50,
-        backgroundColor: 'orange',
-    }
+
+
 });
