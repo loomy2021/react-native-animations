4b27dca3f3359554a4bdff2eb2eaa32b62c66225
loomy2021 <stev@loomcomputer.com>
2024-07-17 15:51:40 +0200

    background removed

--- App.style.js
+++ App.style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const s = StyleSheet.create({
     container: {
 
-        backgroundColor: 'red',
+        // backgroundColor: 'red',
         alignItems: 'center',
         justifyContent: 'center',
     },
